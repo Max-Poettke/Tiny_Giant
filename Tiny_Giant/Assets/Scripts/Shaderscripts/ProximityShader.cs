d6f8d2a770e6299fd103df48185dc1c51bc96014
Max Pöttke <max-poettke@gmx.de>
2024-01-07 23:48:30 +0100

    Added custom shaders and improved Item-handling withing VR scene

--- Tiny_Giant/Assets/Scripts/Shaderscripts/ProximityShader.cs
+++ Tiny_Giant/Assets/Scripts/Shaderscripts/ProximityShader.cs
@@ -10,14 +10,28 @@ public class ProximityShader : MonoBehaviour
     public float maxDistance;
     private Material proximityMaterial;
     public int materialIndex;
+    private BoxCollider collider;
 
     private void Start()
     {
         proximityMaterial = GetComponent<MeshRenderer>().materials[materialIndex];
+        try
+        {
+            collider = GetComponent<BoxCollider>();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+        }
     }
     
     private void Update()
     {
+        if (collider != null)
+        {
+            
+        }
+        
         Vector3 position = originalObjectTransform.position;
         proximityMaterial.SetVector("ObjectPos",position);
         proximityMaterial.SetFloat("MaxDist", maxDistance);
