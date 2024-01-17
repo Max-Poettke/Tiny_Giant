9a79ea6177cffc5cb3fdc79b93caeee19b5db149
Max Pöttke <max-poettke@gmx.de>
2024-01-17 01:33:45 +0100

    Imported VR-Player and normal avatar with network communication. Also implemented custom cropping rendering for the table

--- Tiny_Giant/Assets/PlayerSpawner.cs
+++ Tiny_Giant/Assets/PlayerSpawner.cs
@@ -7,21 +7,24 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     public GameObject playerPrefabPC;
     public GameObject playerPrefabVR;
+    public GameObject pcSpawnPoint;
+    public GameObject vrSpawnPoint;
 
     public void PlayerJoined(PlayerRef player)
     {
-        /*
-        if (player == Runner.LocalPlayer)
-        {
-            Runner.Spawn(playerPrefabPC, new Vector3(0, 1, 1), Quaternion.identity, player);
-            //
-        }
-        */
         
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(playerPrefabVR, new Vector3(0, 1, 0), Quaternion.identity, player);
+            Runner.Spawn(playerPrefabPC, pcSpawnPoint.transform.position, Quaternion.identity, player);
+            //
         }
+        
+        /*
+        if (player == Runner.LocalPlayer)
+        {
+            Runner.Spawn(playerPrefabVR, vrSpawnPoint.transform.position, Quaternion.identity, player);
+        }
+        */
         
     }
 }
