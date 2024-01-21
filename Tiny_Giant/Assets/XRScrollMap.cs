d66af6cc42229b7aea2e0e420a7125721e002d11
Max Pöttke <max-poettke@gmx.de>
2024-01-21 23:42:55 +0100

    Trying to get the map movement for VR working

--- Tiny_Giant/Assets/XRScrollMap.cs
+++ Tiny_Giant/Assets/XRScrollMap.cs
@@ -1,16 +1,74 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
-using UnityEngine.InputSystem.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRScrollMap : MonoBehaviour
 {
-    public XRController controller;
+    public XRController controllerLeft;
+    public NetworkObject networkObject;
+    public NetworkObject levelParent;
+    public List<Material> mapObjectMaterials = new List<Material>();
 
-    // Update is called once per frame
+
+    private Vector3 controllerPositionOld = new Vector3();
+    public Vector3 controllerPositionChange = new Vector3();
+    public bool isGripping = false;
+
+    private float triggerValue;
+
+    private void Start()
+    {
+        //levelParent = GameObject.FindGameObjectWithTag("LevelParent").GetComponent<NetworkObject>();
+        levelParent.RequestStateAuthority();
+    }
+
     void Update()
     {
-        
+        GetControllerInput();
+        if (isGripping && networkObject.HasInputAuthority)
+        {
+            foreach(Material material in mapObjectMaterials)
+            {
+                Vector3 originalPosLeftFrontBottom = material.GetVector("_LeftFrontBottom");
+                Vector3 originalPosRightBackTop = material.GetVector("_RightBackTop");
+                Vector3 newPosLeftFrontBottom = UpdatePosition(originalPosLeftFrontBottom);
+                Vector3 newPosRightBackTop = UpdatePosition(originalPosRightBackTop);
+                material.SetVector("_LeftFrontBottom", newPosLeftFrontBottom);
+                material.SetVector("_RightBackTop", newPosRightBackTop);
+            } 
+            controllerPositionOld = controllerLeft.transform.localPosition;
+            transform.position -= controllerPositionChange;
+        }
+    }
+
+    private void GetControllerInput()
+    {
+        //Check if left controller is trying to grip and move the world
+        if (controllerLeft.inputDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.grip, out triggerValue) &&
+            triggerValue > 0.1f)
+        {
+            isGripping = true;
+            if (controllerPositionOld == Vector3.zero)
+            {
+                controllerPositionOld = controllerLeft.transform.localPosition;
+            }
+            controllerPositionChange = (controllerLeft.transform.localPosition - controllerPositionOld);
+            controllerPositionChange.y = 0f;
+            controllerPositionChange *= Quaternion.Angle(controllerLeft.transform.rotation, transform.rotation);
+            controllerPositionChange *= 0.5f;
+
+        }
+        else
+        {
+            isGripping = false;
+        }
+    }
+    
+    private Vector3 UpdatePosition(Vector3 oldPos)
+    {
+        return oldPos + controllerPositionChange;
     }
 }
