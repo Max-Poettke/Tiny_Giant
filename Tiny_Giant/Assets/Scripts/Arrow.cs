2f14702a2a3dcf321378024fdf0b4842e571324e
Emse <121124119+Onyinyyyye@users.noreply.github.com>
2024-01-24 20:12:12 +0100

    added vanishing effect for arrows, removed spamming

--- Tiny_Giant/Assets/Scripts/Arrow.cs
+++ Tiny_Giant/Assets/Scripts/Arrow.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody rb;
+    private Material material;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        material = gameObject.GetComponent<MeshRenderer>().material;
     }
 
     // Update is called once per frame
@@ -22,5 +25,23 @@ public class Arrow : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void Vanish()
+    {
+        StartCoroutine(VanishC());
+    }
+
+    public IEnumerator VanishC()
+    {
+        var color = material.color;
+        for (float i = 1; i > 0f ; i -= 0.1f)
+        {
+            color.a = i;
+            //gameObject.GetComponent<MeshRenderer>().material.color = color;
+            material.color = color;
+            yield return new WaitForSeconds(0.1f);
+        }
+        Destroy(gameObject);
     }
 }
