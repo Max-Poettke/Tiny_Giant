edf50e379112131d20b9ad6fb92b06b6a192301b
rob3rt-BR <116502158+rob3rt-BR@users.noreply.github.com>
2024-01-29 13:37:55 +0100

    Added activation sound to torches on level 2

--- Tiny_Giant/Assets/Scripts/Audio/AudioManagers/FMODEvents.cs
+++ Tiny_Giant/Assets/Scripts/Audio/AudioManagers/FMODEvents.cs
@@ -11,7 +11,6 @@ public class FMODEvents : MonoBehaviour
     [field: SerializeField] public EventReference bowSounds { get; private set; }
     [field: Header("Ambient")]
     [field: SerializeField] public EventReference natureSounds { get; private set; }
-    [field: SerializeField] public EventReference fire { get; private set; }
     [field:Header("Music")]
     [field: SerializeField] public EventReference music { get; private set; }
     public static FMODEvents eventsInstance { get; private set; }
